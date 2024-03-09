c4d5dccb9e38d849bd5cc21712e6fd0aa6e29a62
Lucas Sarten <lucas@sarten.co.nz>
2024-03-10 01:41:01 +1300

    Add auto categorize

--- app.go
+++ app.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 
-	"github.com/wailsapp/wails/v2/pkg/runtime"
-	"go-budget/internal/parser"
 	dbPkg "go-budget/internal/db"
+	"go-budget/internal/models"
+	"go-budget/internal/parser"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -34,6 +36,15 @@ func (a *App) beforeClose(ctx context.Context) bool {
 	return false
 }
 
+func (a *App) selectFile(ctx context.Context, fileType string) (string, error) {
+	a.ctx = ctx
+	// open file dialog
+	return runtime.OpenFileDialog(ctx, runtime.OpenDialogOptions{
+		Title:   "Select a file",
+		Filters: []runtime.FileFilter{{DisplayName: fileType + " Files", Pattern: "*." + fileType}},
+	})
+}
+
 func (a *App) LoadFile(ctx context.Context, db *dbPkg.Db) {
 	a.ctx = ctx
 	// open file dialog
@@ -45,11 +56,14 @@ func (a *App) LoadFile(ctx context.Context, db *dbPkg.Db) {
 	parser.ImportFile(db, file, "TSB")
 }
 
-func (a *App) selectFile(ctx context.Context, fileType string) (string, error) {
+func (a *App) Categorize(ctx context.Context, db *dbPkg.Db, toCategorize []dbPkg.Transaction) ([]dbPkg.Transaction, error) {
 	a.ctx = ctx
-	// open file dialog
-	return runtime.OpenFileDialog(ctx, runtime.OpenDialogOptions{
-		Title:   "Select a file",
-		Filters: []runtime.FileFilter{{DisplayName: fileType + " Files", Pattern: "*." + fileType}},
-	})
+	// Get transactions from db
+	transactions, err := db.QueryTransactions("SELECT * FROM transactions", nil)
+	if err != nil {
+		return []dbPkg.Transaction{}, err
+	}
+	// Categorize transactions
+	categorized := models.Categorize(transactions, toCategorize)
+	return categorized, nil
 }
---
+++ internal/models/categories.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	dbPkg "go-budget/internal/db"
+
+	"github.com/lithammer/fuzzysearch/fuzzy"
+)
+
+func Categorize(transactions []dbPkg.Transaction, toClassify []dbPkg.Transaction) []dbPkg.Transaction {
+
+	descriptions := make([]string, 0, len(transactions))
+	for _, transaction := range transactions {
+		descriptions = append(descriptions, transaction.Description)
+	}
+
+	for i, transaction := range toClassify {
+		ranks := fuzzy.RankFindNormalizedFold(transaction.Description, descriptions)
+		if len(ranks) > 0 {
+			toClassify[i].Category = transactions[ranks[0].OriginalIndex].Category
+		}
+	}
+
+	return toClassify
+}
